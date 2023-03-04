ac1f16b994e684c7f00c84b3ee8899b9014ce5bb
Anthony <61487785+AnthonyPuggs@users.noreply.github.com>
2023-03-04 14:03:10 +1000

    Create greedflation.py

---
+++ Why Inflation Is Not Caused by Corporate Greed/greedflation.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import scipy
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = (11, 5)
+
+df = pd.read_csv('GreedflationData.csv')
+
+
+fig, ax = plt.subplots(figsize=(10, 5))
+
+x1 = df['Quarter']
+y1 = df['Wages']
+
+plt.plot(x1, y1, label = "Seasonally adjusted Wages")
+
+x2 = df['Quarter']
+y2 = df['CGOP']
+
+plt.plot(x2, y2, label = "Seasonally adjusted Company Gross Operating Profits")
+
+plt.xlabel('Date')
+plt.ylabel('Current Prices ($ Millions)')
+plt.suptitle('Wages and Company Gross Operating Profits across all Australian industries', y=0.96)
+
+plt.legend()
+
+plt.annotate('12.4% decrease in profits',
+            xy=(x2[12], y2[12]),
+            xycoords='data',
+            xytext=(0, -53), 
+            textcoords='offset points',
+            arrowprops=dict(arrowstyle="-"),
+            ha='right') 
+
+text = "Graph created by @AnthonyPuggs using ABS data"
+text_x = 0.595
+text_y = -.08
+
+plt.annotate(text, xy=(text_x, 0), xycoords='axes fraction',
+            xytext=(text_x, text_y), textcoords='axes fraction',
+            fontsize=9, ha='left', va='top')
+
+plt.grid(axis = 'y')
+plt.savefig('AusWageCGOP.png')
+plt.show()
